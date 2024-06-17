12ec43d999b0f2963d1c69d2e479957ea94b0331
jeevss131436 <165214502+jeevss131436@users.noreply.github.com>
2024-06-17 17:35:49 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello World!")
 print("\t This is my first Git repository on GitHub.")
+def print_message(my_name='Python'):
+    print(f"Welcome to the class from {my_name}!)
+
+name = "Julia"
+print_message(name)
